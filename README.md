cab2b9282d32bce57cf5a263b62706b1544e26ef
gzd <gzdaijie@163.com>
2016-03-13 14:29:39 +0800

    add content and copyright

--- README.md
+++ README.md
@@ -9,14 +9,25 @@
 
 > * 自定义博客园markdown渲染样式
 > * 博客园markdown代码没有行号，使用`SyntaxHighlighter`插件重新渲染代码部分，详见[footer.js](./footer.js)，`SyntaxHighlighter`插件博客园已支持。
+> * 博客园markdown不能自动生成目录，因此使用将所有的`<h2>`和`<h3>`提取制作目录，使用`<a>`标签实现目录在页内的跳转
+> * 申明版权信息，实现了在博客正文最顶端自动生成版权信息，出处为当前url。
 > * 更多变化可以参考[我的博客](http://www.cnblogs.com/gzdaijie/p/5187171.html)
 
-### 示例如下
+### 2.1 代码简介
+> * 一个对象，setMyBlog
+> * 四个方法：`代码行号`、`版权信息` 、`超链接新窗口打开`、`自动生成目录章节`
+> * setMyblog.runAll() 为执行所有方法，更改该函数可选择执行某几个方法。
 
+### 2.2 示例如下
+
+#### 2.2.1 更改后的引用
 ![示例1](./image/preview_refer.jpg)
 
+#### 2.2.2 代码行号
 ![示例2](./image/preview_java_code.jpg)
 
+#### 2.2.3 版权信息与目录章节 
+![示例2](./image/preview_content.jpg)
 
 
 ## 3.使用方法
--- footer.js
+++ footer.js
@@ -1,17 +1,66 @@
 <script type="text/javascript">  
-    // 博客内的链接在新窗口打开
-    $("#cnblogs_post_body a").each(function(){
-        $(this).attr("target","_blank");
-    }) 
-    var pre = $("pre.sourceCode"); //选中需要更改的部分
-    if(pre && pre.length){
-        pre.each(function() {
-            var item = $(this);
-            var lang = item.attr("class").split(" ")[1]; //判断高亮的语言
-            item.html(item.html().replace(/<[^>]+>/g,"")); //将<pre>标签中的html标签去掉
-            item.removeClass();   //移除 class
-            item.addClass("brush: " + lang +";"); //根据语言添加笔刷
-            SyntaxHighlighter.all();
-        })
+    var setMyBlog = {
+        setCopyright: function() {
+            //设置版权信息，转载出处自动根据页面url生成
+            var info_str = '<p>作者：<a target="_blank">@gzdaijie</a><br>'+
+                '本文为作者原创，转载请注明出处：<a class="uri"></a></p><hr></hr>', 
+                info = $(info_str),
+                info_a = info.find("a"),
+                url = window.location.href;
+            $(info_a[0]).attr("href","https://github.com/gzdaijie");
+            $(info_a[1]).attr("href",url).text(url);
+            $("#cnblogs_post_body").prepend(info);
+        },
+        setCodeRow: function(){
+            // 代码行号显示
+            var pre = $("pre.sourceCode"); //选中需要更改的部分
+            if(pre && pre.length){
+                pre.each(function() {
+                    var item = $(this);
+                    var lang = item.attr("class").split(" ")[1]; //判断高亮的语言
+                    item.html(item.html().replace(/<[^>]+>/g,"")); //将<pre>标签中的html标签去掉
+                    item.removeClass().addClass("brush: " + lang +";"); //根据语言添加笔刷
+                    SyntaxHighlighter.all();
+                })
+            }
+        },
+        setAtarget: function() {
+            // 博客内的链接在新窗口打开
+            $("#cnblogs_post_body a").each(function(){
+                this.target = "_blank";
+            }) 
+        },
+        setContent: function() {
+            // 根据h2、h3标签自动生成目录
+            var captions_ori = $("#cnblogs_post_body h2"),
+                captions = $("#cnblogs_post_body h2,#cnblogs_post_body h3").clone(),
+                content = $("<blockquote><h4>目录</h4></blockquote>");
+            $("#cnblogs_post_body").prepend(content.append(captions));
+            var index = -1;
+            captions.replaceWith(function(){
+                var self = this;
+                if(self.tagName == "H2" || self.tagName == "h2"){
+                    // 设置点击目录跳转
+                    index += 1;
+                    $('<a name="' + '_caption_' + index + '"></a>').insertBefore(captions_ori[index]); 
+                    return '<a href="#_caption_' + index + '"><strong>' + self.innerHTML + '</strong></a><br>';
+                } else {
+                    return self.innerHTML + "<br>";
+                }
+            });
+        },
+        runAll: function() {
+            /* 运行所有方法
+             * setAtarget() 博客园内标签新窗口打开
+             * setContent() 设置目录
+             * setCopyright() 设置版权信息
+             * setCodeRow() 代码行号显示
+             */ 
+            this.setAtarget(); 
+            this.setContent();
+            this.setCopyright();
+            this.setCodeRow();
+        }
     }
+    setMyBlog.runAll();
 </script>
